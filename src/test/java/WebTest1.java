8fdd56318a8ecc98c2c14dbfc97a46bb966dc79d
TorGash24 <grigorov1986@gmail.com>
2021-10-13 00:46:24 +0700

    add WebTest2

--- src/test/java/WebTest1.java
+++ src/test/java/WebTest1.java
@@ -7,7 +7,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class WbTest {
+public class WebTest1 {
+
     public WebDriver driver;
     public String url = "http://www.99-bottles-of-beer.net/";
     public String driverPathChrome = "D:\\chromedriver_win32/chromedriver.exe";
@@ -52,31 +53,6 @@ public class WbTest {
 
                         // Третья ЧАСТЬ
         Assert.assertEquals(actualResult, expectedResult);
-
-
-
-
     }
-    @Test
-    public void testTwo () throws InterruptedException {
-//        String driverPathChrome = "D:\\chromedriver_win32/chromedriver.exe";
-//        String chromeDriver = "webdriver.chrome.driver";
-//        String url = "http://www.99-bottles-of-beer.net/";
-        String expectedResult = "Welcome to 99 Bottles of Beer";
-//        System.setProperty(chromeDriver, driverPathChrome);
-        // Создаем драйвер, чтобы он начал выполнять действия
-//        WebDriver driver = new ChromeDriver();git
 
-        // Открываем страницу http
-        driver.get(url);
-        // Выставляем задержку в 3 сек
-        Thread.sleep(3000);
-        // Находим заголовок, который будет являться результатом теста
-        WebElement actualResult = driver.findElement(By.xpath("//div[@id='main']//h2[text()='Welcome to 99 Bottles of Beer']"));
-        // Сравниваем - достаем с заголовка текс и сравниваем его с expectedResult.
-        Assert.assertEquals(actualResult.getText(), "Welcome to 99 Bottles of Beer");
-        System.out.println(url);
-
-
-    }
 }
